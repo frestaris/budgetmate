9843e6607520cf144dc4610816bb7a2e30783bc1
Aris <frestaris@gmail.com>
2024-11-13 20:48:00 +1000

    complete singup page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,50 @@
+import { Button, Label, TextInput } from "flowbite-react";
+import { Link } from "react-router-dom";
+
 function SignUp() {
-  return <div>signup</div>;
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 text-cyan-700 rounded-lg">
+              BudgetMate
+            </span>
+          </Link>
+          <p className="text-md mt-5">
+            BudgetMate helps you manage your finances and stay organized, with
+            tools for budget planning, expense tracking, and an agenda to keep
+            your contacts and important details at hand.
+          </p>
+        </div>
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Your username" />
+              <TextInput type="text" placeholder="Username" id="username" />
+            </div>
+            <div>
+              <Label value="Your email" />
+              <TextInput type="text" placeholder="name@email.com" id="email" />
+            </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput type="text" placeholder="Password" id="password" />
+            </div>
+            <Button gradientDuoTone="greenToBlue" type="submit">
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to="/sign-in" className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
