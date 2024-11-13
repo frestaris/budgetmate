4e01bb894ecad6027ea398f1a977c3bbb16e59a0
Aris <frestaris@gmail.com>
2024-11-13 21:02:44 +1000

    add functionality to the sign up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,13 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer position="top-center" autoClose={2000} />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
