5a2ba398587a5c35d960802491b6d66b3dcbafc1
Aris <frestaris@gmail.com>
2024-11-17 12:24:24 +1000

    show the total formatted by frequency and category

--- client/src/components/BudgetPlanner.jsx
+++ client/src/components/BudgetPlanner.jsx
@@ -14,14 +14,17 @@ import { MdLocalGroceryStore } from "react-icons/md";
 import { Button, Select, TextInput } from "flowbite-react";
 
 import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
 
 function BudgetPlanner() {
+  const { currentUser } = useSelector((state) => state.user);
   const [openPanels, setOpenPanels] = useState([]);
   const [description, setDescription] = useState("");
   const [amount, setAmount] = useState("");
   const [category, setCategory] = useState("");
   const [frequency, setFrequency] = useState("");
-  const [budgetItems, setBudgetItems] = useState([]);
+  const [userBudgets, setUserBudgets] = useState([]);
+  const [totalFrequency, setTotalFrequency] = useState("annually");
 
   const categories = [
     { name: "Income", icon: <FaHandHoldingUsd className="w-6 h-6" /> },
@@ -46,24 +49,93 @@ function BudgetPlanner() {
     });
   };
 
-  useEffect(() => {
-    getBudgets();
-  }, []);
-
-  const getBudgets = async () => {
-    try {
-      const response = await fetch("/api/budgets/getbudgets");
-      const data = await response.json();
-      if (response.ok) {
-        setBudgetItems(data.budgets);
-      } else {
-        alert(`Error: ${data.message}`);
-      }
-    } catch (error) {
-      console.error("Error fetching budgets:", error);
-      alert("Error fetching budgets. Please try again.");
-    }
+  const formattedAmount = (amount) => {
+    return new Intl.NumberFormat("en-US", {
+      style: "decimal",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(amount);
   };
+
+  const getTotalByCategory = (category) => {
+    return userBudgets
+      .filter((budget) => budget.category === category)
+      .reduce((total, budget) => total + budget.amount, 0);
+  };
+
+  const convertAmountByFrequency = (
+    amount,
+    originalFrequency,
+    targetFrequency
+  ) => {
+    const frequencyMapping = {
+      weekly: {
+        fortnightly: amount * 2,
+        monthly: amount * 4,
+        annually: amount * 52,
+      },
+      fortnightly: {
+        weekly: amount / 2,
+        monthly: (amount * 26) / 12,
+        annually: amount * 26,
+      },
+      monthly: {
+        weekly: amount / 4,
+        fortnightly: (amount * 12) / 26,
+        annually: amount * 12,
+      },
+      annually: {
+        weekly: amount / 52,
+        fortnightly: amount / 26,
+        monthly: amount / 12,
+      },
+    };
+
+    return frequencyMapping[originalFrequency]?.[targetFrequency] || amount;
+  };
+
+  const getTotalByFrequency = (frequency) => {
+    const totalIncome = userBudgets
+      .filter((budget) => budget.category === "income")
+      .reduce((total, budget) => {
+        const convertedAmount = convertAmountByFrequency(
+          budget.amount,
+          budget.frequency,
+          frequency
+        );
+        return total + convertedAmount;
+      }, 0);
+
+    const totalExpenses = userBudgets
+      .filter((budget) => budget.category !== "income")
+      .reduce((total, budget) => {
+        const convertedAmount = convertAmountByFrequency(
+          budget.amount,
+          budget.frequency,
+          frequency
+        );
+        return total + Math.abs(convertedAmount);
+      }, 0);
+
+    return totalIncome - totalExpenses;
+  };
+  useEffect(() => {
+    const fetchBudgets = async () => {
+      try {
+        const res = await fetch("/api/budgets/getbudgets");
+        const data = await res.json();
+        console.log(currentUser._id);
+        console.log(data.budgets);
+
+        if (res.ok && data.budgets) {
+          setUserBudgets(data.budgets);
+        }
+      } catch (error) {
+        console.log("Error fetching budgets:", error);
+      }
+    };
+    fetchBudgets();
+  }, [currentUser._id]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -73,9 +145,16 @@ function BudgetPlanner() {
       return;
     }
 
+    const amountValue = parseFloat(amount);
+    if (isNaN(amountValue)) {
+      alert("Please enter a valid numeric amount.");
+      return;
+    }
+    const adjustedAmount = category === "income" ? amountValue : -amountValue;
+
     const budgetData = {
       description,
-      amount,
+      amount: adjustedAmount,
       category,
       frequency,
     };
@@ -92,7 +171,7 @@ function BudgetPlanner() {
       const data = await response.json();
       if (response.ok) {
         toast.success("Budget item added successfully!");
-        setBudgetItems((prevItems) => [
+        setUserBudgets((prevItems) => [
           ...prevItems,
           { ...budgetData, id: data.id },
         ]);
@@ -122,7 +201,7 @@ function BudgetPlanner() {
 
       if (response.ok) {
         toast.success("Budget Deleted");
-        setBudgetItems(data.budgets);
+        setUserBudgets(data.budgets);
       } else {
         alert(`Error: ${data.message}`);
       }
@@ -202,7 +281,13 @@ function BudgetPlanner() {
                 <span>{category.name}</span>
               </div>
               <div className="flex items-center gap-2">
-                <span>0.00$</span>
+                <span>
+                  {formattedAmount(
+                    getTotalByCategory(category.name.toLowerCase())
+                  )}
+                  $
+                </span>
+
                 {openPanels.includes(index) ? (
                   <FaChevronUp className="w-4 h-4" />
                 ) : (
@@ -210,11 +295,10 @@ function BudgetPlanner() {
                 )}
               </div>
             </button>
-
             {openPanels.includes(index) && (
               <div className="p-4">
                 <ul>
-                  {budgetItems
+                  {userBudgets
                     .filter(
                       (item) => item.category === category.name.toLowerCase()
                     )
@@ -224,8 +308,9 @@ function BudgetPlanner() {
                           <p>{item.description}</p>
                         </div>
                         <div className="flex items-center gap-2">
-                          <p>{item.amount}$</p>
-                          <p>({item.frequency})</p>
+                          <p>
+                            ${formattedAmount(item.amount)} ({item.frequency})
+                          </p>
                           <FaRegTrashAlt
                             color="red"
                             className="cursor-pointer"
@@ -239,6 +324,27 @@ function BudgetPlanner() {
             )}
           </div>
         ))}
+        <div className="w-full flex justify-between items-center p-4 rounded-md dark:bg-gray-700 bg-gray-50">
+          <div className="flex items-center gap-2">
+            <span>TOTAL</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <span>${formattedAmount(getTotalByFrequency(totalFrequency))}</span>
+            <Select
+              id="totalFrequency"
+              value={totalFrequency}
+              onChange={(e) => setTotalFrequency(e.target.value)}
+            >
+              <option value="" disabled>
+                Select Frequency
+              </option>
+              <option value="weekly">Weekly</option>
+              <option value="fortnightly">Fortnightly</option>
+              <option value="monthly">Monthly</option>
+              <option value="annually">Annually</option>
+            </Select>
+          </div>
+        </div>
       </div>
     </div>
   );
