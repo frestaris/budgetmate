1b6e56219737dbfa6346ffcbe7aa03c1a5c3df42
Aris <frestaris@gmail.com>
2024-12-14 16:40:08 +1000

    added dashStats

--- client/src/components/DashHome.jsx
+++ client/src/components/DashHome.jsx
@@ -1,10 +1,12 @@
 import BarChart from "./BarChart";
+import DashStats from "./DashStats";
 import DoughnutChart from "./DoughnutChart";
 import PieChart from "./PieChart";
 
 function DashHome() {
   return (
     <div className="flex flex-col items-center w-full gap-4">
+      <DashStats />
       <div className="flex flex-col custom:flex-row justify-center gap-4">
         <DoughnutChart />
         <PieChart />
